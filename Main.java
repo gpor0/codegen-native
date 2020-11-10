12b1768ec2b212e94790027be37f1bfe16b233a0
Gregor <gpor@icloud.com>
2020-11-10 18:05:15 +0100

    minor

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
